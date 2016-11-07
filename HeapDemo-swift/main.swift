0387e53ff4d658c5de68e8c215159218255c0d7b
Aldrin Martoq <aldrin@martoq.cl>
2016-11-07 18:20:17 -0300

    Initial Commit

---
+++ HeapDemo-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HeapDemo-swift
+//
+//  Created by Aldrin Martoq on 11/7/16.
+//  Copyright © 2016 Servicios A0 SpA. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
